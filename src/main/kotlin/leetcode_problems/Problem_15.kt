d976b5cd541abf9bce0c4b0f9d5e5f62d9fe3541
Javokhir Savriev <j.savriev@aigroup.uz>
2022-12-19 20:01:41 +0500

    Solved 15 and 344 problems

---
+++ src/main/kotlin/leetcode_problems/Problem_15.kt
@@ -0,0 +1,46 @@
+package leetcode_problems
+
+import java.util.*
+
+
+fun main() {
+    println(threeSum(intArrayOf(-1, 0, 1, 2, -1, -4)))
+}
+
+fun threeSum(nums: IntArray): List<List<Int>> {
+    Arrays.sort(nums)
+
+    val result: MutableSet<List<Int>> = HashSet()
+
+    for (i in 0 until nums.size - 1) {
+        val one = nums[i]
+
+        for (j in i until nums.size) {
+            if (i == j) continue
+
+            val two = nums[j]
+            val three = 0 - one - two
+            val idxThree = Arrays.binarySearch(nums, three)
+
+            if (idxThree >= 0 && idxThree != i && idxThree != j) {
+                val list: MutableList<Int> = ArrayList()
+                if (three <= one) {
+                    list.add(three)
+                    list.add(one)
+                    list.add(two)
+                } else if (three <= two) {
+                    list.add(one)
+                    list.add(three)
+                    list.add(two)
+                } else {
+                    list.add(one)
+                    list.add(two)
+                    list.add(three)
+                }
+                result.add(list)
+            }
+        }
+    }
+
+    return result.toList()
+}
---
+++ src/main/kotlin/leetcode_problems/Problem_344.kt
@@ -0,0 +1,24 @@
+package leetcode_problems
+
+/*
+    Solution: Mid-pointer of the array is found.
+    The elements on the left and right sides of the mid-point are replaced.
+ */
+
+fun main() {
+    reverseString(charArrayOf('h', 'e', 'l', 'l', '0'))
+}
+
+fun reverseString(s: CharArray) {
+    val midPoint = s.size / 2
+
+    for (index in 0 until midPoint) {
+        val temp = s[index]
+        s[index] = s[s.lastIndex - index]
+        s[s.lastIndex - index] = temp
+    }
+}
+
+//fun reverseString(s: CharArray) {
+//    s.reverse()
+//}
