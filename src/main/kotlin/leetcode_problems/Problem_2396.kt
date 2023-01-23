7535d77831f0bc0ae0654da0bc99992c7b31befe
Javokhir Savriev <j.savriev@aigroup.uz>
2023-01-23 11:08:20 +0500

    Updated

---
+++ src/main/kotlin/leetcode_problems/Problem_2396.kt
@@ -0,0 +1,14 @@
+package leetcode_problems
+
+import utils.print
+
+fun main() {
+    "Strictly Palindromic Number solution" print {
+        println(solution(9))
+        println(solution(4))
+    }
+}
+
+private fun solution(n: Int): Boolean {
+    return false
+}
