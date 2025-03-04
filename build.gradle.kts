8bf029823b3825f4539dd2f524eee3d205ddad02
Javokhir <javokhirdev@gmail.com>
2025-03-04 08:50:53 +0500

    Updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 group = "uz.javokhirdev"
@@ -10,6 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
 }
--- src/main/kotlin/util/Convert.kt
+++ src/main/kotlin/util/Convert.kt
@@ -1,21 +1,59 @@
 package util
 
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
 fun main() {
-    val uzLines = getResourceAsText("text_uz.txt")?.split("\n\n").orEmpty()
-    val cyrLines = getResourceAsText("text.txt")?.split("\n\n").orEmpty()
+//    val uzLines = getResourceAsText("text_uz.txt")?.split("\n\n").orEmpty()
+//    val cyrLines = getResourceAsText("text.txt")?.split("\n\n").orEmpty()
+//
+//    uzLines.forEachIndexed { index, uz ->
+//        val content = """
+//                {
+//                    "_id": ${index + 1},
+//                    "uz": "$uz",
+//                    "uz_cyr": "${cyrLines[index]}",
+//                },
+//            """.trimIndent()
+//
+//        println(content)
+//    }
+//
+//    val arabic = getResourceAsText("hafs.json").orEmpty()
+//    val arabicMap: Map<String, Arabic> = json.decodeFromString(arabic)
+//
+//    val transUz = getResourceAsText("translation_uz.json").orEmpty()
+//    val transUzMap: Map<String, Translation> = json.decodeFromString(transUz)
+//
+//    val transUzCyr = getResourceAsText("translation_uz_cyr.json").orEmpty()
+//    val transUzCyrMap: Map<String, Translation> = json.decodeFromString(transUzCyr)
+//
+//    val transEnCyr = getResourceAsText("translation_en.json").orEmpty()
+//    val transEnMap: Map<String, Translation> = json.decodeFromString(transEnCyr)
+//
+//    val transRuCyr = getResourceAsText("translation_ru.json").orEmpty()
+//    val transRuMap: Map<String, Translation> = json.decodeFromString(transRuCyr)
+//
+//    val translitEn = getResourceAsText("translit.en.csv").orEmpty().split("\n")
+//    val translitRu = getResourceAsText("translit.ru.csv").orEmpty().split("\n")
 
-    uzLines.forEachIndexed { index, uz ->
-        val content = """
-                {
-                    "_id": ${index + 1},
-                    "uz": "$uz",
-                    "uz_cyr": "${cyrLines[index]}",
-                },
-            """.trimIndent()
-
-        println(content)
-    }
+//    val json = Json { ignoreUnknownKeys = true }
+//
+//    val duaItemsJson = getResourceAsText("dua.json").orEmpty()
+//    val duasItems: List<DuaRequest> = json.decodeFromString(duaItemsJson)
+//
+//    duasItems.forEachIndexed { index, dua ->
+//        val jsonString = json.encodeToString(
+//            dua.copy(
+//                id = 319 + index,
+//                categoryId = 13,
+//                chapterId = 184 + index
+//            )
+//        )
+//
+//        println("$jsonString,")
+//    }
 }
 
 fun getResourceAsText(path: String): String? =
-    object {}.javaClass.getResource(path)?.readText()
+    { }::class.java.classLoader.getResource(path)?.readText()
